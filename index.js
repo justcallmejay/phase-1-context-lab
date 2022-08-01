791c0bf414e8e1562e95ee2efb98a4d27fda5675
jay <jaypifiverr@gmail.com>
2022-08-01 15:08:06 -0400

    commit tree 98347da15b54479249a2121ae8ca1bac12e9d54a

--- index.js
+++ index.js
@@ -1,4 +1,41 @@
 /* Your Code Here */
+function createEmployeeRecord(employee) {
+    
+    return {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+createEmployeeRecord(["Grey", "Worm", "Security", 1])
+
+function createEmployeeRecords(employees) {
+    return employees.map(employee => {
+        return createEmployeeRecord(employee)
+    })
+}
+
+//When passing at indexText.js the two arrays need to be inside the brackets; otherwise, .map will iterate through each index in each string in each element
+
+function createTimeInEvent (event) {
+    let splitRec = event.split(' ')
+    let [date, hour] = splitRec
+
+    let newEvent = {
+        type: "TimeIn",
+        date: date,
+        hour: hour
+    }
+
+    return (this.timeInEvents.push(newEvent))
+}
+
+createTimeInEvent("2014-02-28 1400")
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
